c23d68e382c6a837bf0f00d60db2c7fe571f3b5c
starr-r <128701403+starr-r@users.noreply.github.com>
2024-05-19 12:20:31 +0800

    2

---
+++ springboot/src/main/java/com/example/demo/mapper/ArtifactMapper.java
@@ -0,0 +1,39 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.Artifact;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.*;
+
+@Repository
+@Mapper
+public interface ArtifactMapper {
+
+    @Select("Select id,relicTime,library_Chinese,imageUrl,material_Chinese, artifactName_Chinese, size_Chinese, description_Chinese from artifact where artifactName like CONCAT('%', #{artifactname}, '%') or artifactName_Chinese like CONCAT('%', #{artifactname}, '%')")
+    public List<Artifact> findByArtifactName(String artifactname);
+
+    public List<Artifact> DeepSearch(String s);
+
+    @Select("Select id,relicTime,library_Chinese,imageUrl,material_Chinese, artifactName_Chinese, size_Chinese, description_Chinese from artifact where library like CONCAT('%',#{libraryName},'%') or library_Chinese like CONCAT('%',#{libraryName},'%') ")
+    public List<Artifact> findByLibraryName(String libraryName);
+
+    @Select("Select id,relicTime,library_Chinese,imageUrl,material_Chinese, artifactName_Chinese, size_Chinese, description_Chinese from artifact where relicTime like CONCAT('%',#{relicTime},'%')")
+    public List<Artifact> findByRelicTime(String relicTime);
+
+    @Select("select id,relicTime,library_Chinese,imageUrl, material_Chinese,artifactName_Chinese, size_Chinese, description_Chinese from artifact where id=#{artifactId}")
+    public Artifact findById(Integer artifactId);
+
+    @Select("SELECT id,relicTime,library_Chinese,imageUrl, material_Chinese,artifactName_Chinese, size_Chinese, description_Chinese FROM artifact ")
+    public List<Artifact> findAllRandom();
+
+    @Select("select id,relicTime,library_Chinese,imageUrl, material_Chinese,artifactName_Chinese, size_Chinese, description_Chinese from artifact where " +
+            "material_Chinese like CONCAT('%',#{materialChinese},'%') " +
+            "or library_Chinese=#{libraryChinese} " +
+            "or relicTime_chinese=#{libraryChinese} " +
+            "and id !=#{id} order by rand() LIMIT 8 ")
+    public List<Artifact> findVague(Artifact artifact);
+}
+
---
+++ springboot/src/main/java/com/example/demo/mapper/UserMapper.java
@@ -0,0 +1,33 @@
+package com.example.demo.mapper;
+
+import com.example.demo.entity.User;
+import org.apache.ibatis.annotations.*;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@Mapper
+public interface UserMapper {
+    @Select("Select * from user where id=#{id}")
+    public User findById(Integer id);
+    @Update("update user set " +
+            "username=#{username}," +
+            "password=#{password}," +
+            "avatar_url=#{avatarUrl}," +
+            "email=#{email}," +
+            "phone=#{phone}," +
+            "update_time=#{updateTime}," +
+            "sex=#{sex}," +
+            "age=#{age} " +
+            "where id=#{id}")
+    public void updateInfo( User user);
+    @Insert("Insert into user(username,password,create_time,isbanned,avatar_url) values (#{username},#{password},#{createTime},#{isBanned},'https://img.touxiangwu.com/zb_users/upload/2023/04/202304061680747835345417.jpg')")
+    public void insert(User user);
+    @Select("select * from user where username=#{username};")
+    public User findByName(String username);
+    @Select("select password from user where username=#{username};")
+    public String findPasswordByName(String username);
+    @Select("select password from user where id=#{id};")
+    public String findPasswordById(Integer id);
+    @Update("update user set isbanned=true where id=#{UserId} ")
+    public void updateIsBannedByUserId(Integer UserId);
+}
